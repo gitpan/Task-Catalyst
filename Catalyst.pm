8bed7d72757e127c7522b5ecb8452f0b8a9e781d
Sebastian Riedel <kraihx@googlemail.com>
2005-11-21 03:22:49 +0000

    Import of SRI/Task-Catalyst-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Catalyst
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SRI/Task-Catalyst-0.01.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

---
+++ Catalyst.pm
@@ -0,0 +1,66 @@
+package Task::Catalyst;
+
+use strict;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+Task::Catalyst - All you need to start with Catalyst
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Task::Catalyst'>
+
+=head1 DESCRIPTION
+
+Installs everything you need to write serious Catalyst applications.
+
+L<Catalyst>
+
+L<Catalyst::Plugin::FormValidator>
+
+L<Catalyst::Plugin::FormValidator::Simple>
+
+L<Catalyst::Plugin::FillInForm>
+
+L<Catalyst::Plugin::Prototype>
+
+L<Catalyst::Plugin::Singleton>
+
+L<Catalyst::Plugin::DefaultEnd>
+
+L<Catalyst::Plugin::SubRequest>
+
+L<Catalyst::Plugin::Session>
+
+L<Catalyst::Plugin::Session::Store::File>
+
+L<Catalyst::Plugin::Session::State::Cookie>
+
+L<Catalyst::Plugin::Session::State::URI>
+
+L<Catalyst::Model::DBIC::Plain>
+
+L<Catalyst::Model::DBIC>
+
+L<Catalyst::View::TT>
+
+L<Test::WWW::Mechanize::Catalyst>
+
+L<DBD::SQLite>
+
+L<YAML>
+
+=head1 AUTHOR
+
+Sebastian Riedel, C<sri@oook.de>
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
+1;
