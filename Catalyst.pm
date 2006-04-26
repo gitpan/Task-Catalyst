34b5c4418fd6d8cd20a153608b3d65b068aaae4e
Sebastian Riedel <kraihx@googlemail.com>
2006-04-26 22:23:00 +0000

    Import of SRI/Task-Catalyst-1.90 from CPAN.
    gitpan-cpan-distribution: Task-Catalyst
    gitpan-cpan-version:      1.90
    gitpan-cpan-path:         SRI/Task-Catalyst-1.90.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

--- Catalyst.pm
+++ Catalyst.pm
@@ -2,11 +2,11 @@ package Task::Catalyst;
 
 use strict;
 
-our $VERSION = '1.80';
+our $VERSION = '1.90';
 
 =head1 NAME
 
-Task::Catalyst - DEPRECATED!
+Task::Catalyst - All you need to start with Catalyst
 
 =head1 SYNOPSIS
 
@@ -14,7 +14,55 @@ C<perl -MCPAN -e 'install Task::Catalyst'>
 
 =head1 DESCRIPTION
 
-This module is now deprecated, there will be a replacement with a different name soon.
+Installs everything you need to write serious Catalyst applications.
+
+L<Catalyst>
+
+L<Catalyst::Log::Log4perl>
+
+L<Catalyst::Plugin::HTML::Widget>
+
+L<Catalyst::Plugin::StackTrace>
+
+L<Catalyst::Plugin::Prototype>
+
+L<Catalyst::Plugin::Singleton>
+
+L<Catalyst::Plugin::DefaultEnd>
+
+L<Catalyst::Plugin::SubRequest>
+
+L<Catalyst::Plugin::Session>
+
+L<Catalyst::Plugin::Session::Store::File>
+
+L<Catalyst::Plugin::Session::State::Cookie>
+
+L<Catalyst::Plugin::Session::State::URI>
+
+L<Catalyst::Plugin::Authentication>
+
+L<Catalyst::Plugin::Authentication::Store::DBIC>
+
+L<Catalyst::Plugin::Authentication::Store::Htpasswd>
+
+L<Catalyst::Plugin::Authorization::ACL>
+
+L<Catalyst::Plugin::Authorization::Roles>
+
+L<Catalyst::Plugin::I18N>
+
+L<Catalyst::Plugin::XMLRPC>
+
+L<Catalyst::Plugin::Pluggable>
+
+L<Catalyst::Model::DBIC::Schema>
+
+L<Catalyst::View::TT>
+
+L<Test::WWW::Mechanize::Catalyst>
+
+L<DBD::SQLite>
 
 =head1 AUTHOR
 
